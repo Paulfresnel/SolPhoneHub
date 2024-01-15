fd2034fbb9db6ecade2a9ac35fa1b04f705f1604
Paul Fresnel <paul.fresnel1@gmail.com>
2024-01-15 19:34:07 +0100

    test

--- src/components/NftAirdropsCalculator/NftAirdropsCalculator.jsx
+++ src/components/NftAirdropsCalculator/NftAirdropsCalculator.jsx
@@ -12,21 +12,34 @@ function NftAirdropsCalculator(props){
 
     const [isLoading, setIsLoading] = useState(false);
 
+    const serverUrl = process.env.REACT_APP_SERVER_URL;
+    
+
     useEffect(()=>{
+        console.log("serv url:", serverUrl)
         const fetchNfts = async ( ) =>{
             setIsLoading(true);
-            const nftListing = await Axios.get('https://api-mainnet.magiceden.dev/v2/collections');
-            const nftsArray = nftListing.data;
-            let filteredArray = nftsArray.filter((x) =>{
-                if (x.symbol === "sagapunks" || x.symbol === "sagamonkes" || x.symbol === "sagasharkyclub"){
+            const response = await Axios.get(`${serverUrl}/api/nft`);
+            console.log("raw backend data fetched:", response);
+            const nftsArray = response.data.nft;
+            let filteredArray = nftsArray.filter((x) => {
+                if (x.symbol === "sagapunks" || x.symbol === "sagamonkes" || x.symbol === "saga_dawgs" ||
+                 x.symbol === "saga_rpg"  || x.symbol === "sagasharkyclub" || x.symbol === "slimesofsaga"
+                 || x.symbol === "saga_aliens" || x.symbol === "saga_cats" || x.symbol === "sentients" 
+                 || x.symbol === "sagagoatclub"  ){
                     return x;
                 }
             })
             
             let completeNftsArr = filteredArray.map(async (nft)=>{
-                let marketData = await Axios.get(`https://api-mainnet.magiceden.dev/v2/collections/${nft.symbol}/stats`);
-                nft.floorPrice = marketData.data.floorPrice;
+                try{
+                let response = await Axios.get(`${serverUrl}/api/nft/${nft.symbol}`);
+                console.log("fetched successfully backend data for individual nft:", response.data.nftData)
+                nft.floorPrice = response.data.nftData.floorPrice;
                 return nft;
+            } catch (err){
+                console.log("Error has occured", err)
+            }
             })
             const updatedNftsArr = await Promise.all(completeNftsArr);
             await setAirdroppedNftsData(updatedNftsArr);
@@ -58,9 +71,9 @@ function NftAirdropsCalculator(props){
                         <Divider/>
                         <CardFooter >
                             <ButtonGroup spacing={"50"} >
-                                <a rel="noreferrer" target="_blank" href={nft.twitter}><i class="fa-brands fa-twitter"></i></a>
-                                <a rel="noreferrer" target="_blank" href={nft.discord}><i class="fa-brands fa-discord"></i></a>
-                                <a rel="noreferrer" target="_blank" href={nft.website}><i class="fa-solid fa-globe"></i></a>
+                                <a rel="noreferrer" target="_blank" href={nft.twitter}><i className="fa-brands fa-twitter"></i></a>
+                                <a rel="noreferrer" target="_blank" href={nft.discord}><i className="fa-brands fa-discord"></i></a>
+                                <a rel="noreferrer" target="_blank" href={nft.website}><i className="fa-solid fa-globe"></i></a>
                             </ButtonGroup>
                         </CardFooter>
                     </Card>
